15e76b0ef20c6e108d7c0e9910fa5927532b1277
flyinv1 <flyinv1@gmail.com>
2020-10-16 00:34:45 -0700

    clean up data analysis -> switch to py

---
+++ Simulator/Data Analysis/hot_analysis.py
@@ -0,0 +1,15 @@
+"""
+    Compute performance metrics from hot fire data
+
+    Metrics:
+        Isp
+        F (avg)
+        Pc (avg)
+        Cf
+        mdot_ox
+        mdot_f
+"""
+
+# This parameter is measured post-burn by weighing the fuel grain
+delta_m_f_act = 0
+
